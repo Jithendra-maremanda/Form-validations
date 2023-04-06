7ff49649a9e988c11d3fe00792b48ab3adba915f
Jithendra-maremanda <103123796+Jithendra-maremanda@users.noreply.github.com>
2023-04-06 10:39:08 +0530

    first

---
+++ lib/FormPage.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'InputDeco_design.dart';
+
+class FormPage extends StatefulWidget {
+  @override
+  _FormPageState createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  late String name, email, phone;
+
+  //TextController to read text entered in text field
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmpassword = TextEditingController();
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.person, "Full Name"),
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'Please Enter Name';
+                      }
+                      return null;
+                    },
+                    onSaved: (String? value) {
+                      name = value!;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.email, "Email"),
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'Please a Enter';
+                      }
+                      if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                          .hasMatch(value)) {
+                        return 'Please a valid Email';
+                      }
+                      return null;
+                    },
+                    onSaved: (String? value) {
+                      email = value!;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    controller: password,
+                    keyboardType: TextInputType.text,
+                    decoration: buildInputDecoration(Icons.lock, "Password"),
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'Please a Enter Password';
+                      }
+                      if (!RegExp(
+                              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                          .hasMatch(value)) {
+                        return 'Please a valid Password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    controller: confirmpassword,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                    decoration:
+                        buildInputDecoration(Icons.lock, "Confirm Password"),
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return 'Please re-enter password';
+                      }
+                      print(password.text);
+
+                      print(confirmpassword.text);
+
+                      if (password.text != confirmpassword.text) {
+                        return "Password does not match";
+                      }
+
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    // color: Colors.redAccent,
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        print("Successful");
+
+                        return;
+                      } else {
+                        print("UnSuccessfull");
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
